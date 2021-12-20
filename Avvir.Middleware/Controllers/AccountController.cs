1fc8674ade7abc86613b3fdcf7e261784c508941
Виктор Клейн <vkleyn@ibs.ru>
2021-12-20 17:39:02 +0400

    WebApi project added, connected with the DataLayer project.

---
+++ Avvir.Middleware/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Avvir.DataLayer;
+using System.Collections;
+
+namespace Avvir.Middleware.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [HttpPost]
+        public void RegisterAccount()
+        {
+            var db = new AvvirModel();
+            var account = new Account();
+
+            //insert logic
+            
+        }
+
+        [HttpPatch]
+        public void UpdateAccount([FromUri] string UIN)
+        {
+            var db = new AvvirModel();
+            var account = db.Account.FirstOrDefault(a => a.UIN == UIN);
+
+            if (account != null)
+            {
+                //update logic
+            }
+
+        }
+    }
+}
