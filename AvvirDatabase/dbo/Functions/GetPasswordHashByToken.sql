18f6b5ff1a8fb56aee468009436b8cc8f9a43b64
Victor Klein <vikklein93@gmail.com>
2022-01-05 16:59:53 +0400

    Added new database objects related to authentication feature.

---
+++ AvvirDatabase/dbo/Functions/GetPasswordHashByToken.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		Victor Klein
+-- Create date: 05.01.2022
+-- Description:	Return account's password hash by token
+-- =============================================
+CREATE FUNCTION GetPasswordHashByToken (@token UNIQUEIDENTIFIER)
+RETURNS UNIQUEIDENTIFIER
+AS
+BEGIN
+	DECLARE @result UNIQUEIDENTIFIER
+		   ,@account UNIQUEIDENTIFIER
+
+	SELECT
+		@account = AccountGUID
+	FROM Token t
+	WHERE t.[GUID] = @token
+
+	SELECT
+		@result = a.PasswordHash
+	FROM Account a
+	WHERE a.[GUID] = @account
+
+	RETURN @result
+
+END
