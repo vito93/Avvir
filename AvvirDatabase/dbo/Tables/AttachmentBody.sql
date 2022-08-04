0cd45c9b665d061ee6c8373b0685b7ac3f14c43d
Victor Klein <vikklein93@gmail.com>
2022-08-04 22:27:11 +0400

    Added tables for attachments and groups, some Message columns.

---
+++ AvvirDatabase/dbo/Tables/AttachmentBody.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[AttachmentBody] (
+    [GUID]       UNIQUEIDENTIFIER CONSTRAINT [DF_AttachmentBody_GUID] DEFAULT (newsequentialid()) NOT NULL,
+    [HeaderGUID] UNIQUEIDENTIFIER NOT NULL,
+    [Body]       VARBINARY (MAX)  NOT NULL,
+    CONSTRAINT [PK_AttachmentBody] PRIMARY KEY CLUSTERED ([GUID] ASC)
+);
+
--- AvvirDatabase/dbo/Tables/Message.sql
+++ AvvirDatabase/dbo/Tables/Message.sql
@@ -1,14 +1,19 @@
 ﻿CREATE TABLE [dbo].[Message] (
-    [GUID]         UNIQUEIDENTIFIER CONSTRAINT [DF_Message_GUID] DEFAULT (newsequentialid()) NOT NULL,
-    [AuthorGUID]   UNIQUEIDENTIFIER NOT NULL,
-    [ReceiverGUID] UNIQUEIDENTIFIER NULL,
-    [MessageBody]  NVARCHAR (MAX)   NOT NULL,
-    [CreateDate]   DATETIME2 (7)    CONSTRAINT [DF_Message_CreateDate] DEFAULT (getdate()) NOT NULL,
-    [GroupGUID]    UNIQUEIDENTIFIER NULL,
+    [GUID]             UNIQUEIDENTIFIER CONSTRAINT [DF_Message_GUID] DEFAULT (newsequentialid()) NOT NULL,
+    [AuthorGUID]       UNIQUEIDENTIFIER NOT NULL,
+    [ReceiverGUID]     UNIQUEIDENTIFIER NULL,
+    [MessageBody]      NVARCHAR (MAX)   NOT NULL,
+    [CreateDate]       DATETIME2 (7)    CONSTRAINT [DF_Message_CreateDate] DEFAULT (getdate()) NOT NULL,
+    [GroupGUID]        UNIQUEIDENTIFIER NULL,
+    [ReplyMessageGUID] UNIQUEIDENTIFIER NULL,
+    [Created]          DATETIME2 (3)    NULL,
+    [Edited]           DATETIME2 (3)    NULL,
     CONSTRAINT [PK_Message] PRIMARY KEY CLUSTERED ([GUID] ASC),
     CONSTRAINT [FK_Author] FOREIGN KEY ([AuthorGUID]) REFERENCES [dbo].[Account] ([GUID]),
     CONSTRAINT [FK_Receiver] FOREIGN KEY ([ReceiverGUID]) REFERENCES [dbo].[Account] ([GUID])
 );
+
+
 
 
 
