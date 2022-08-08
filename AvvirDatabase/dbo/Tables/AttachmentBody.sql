f3529c7eb63ff2fc94dc16cf2d4c965ed60ca6b8
Victor Klein <vikklein93@gmail.com>
2022-08-09 01:17:59 +0400

    Added FILESTREAM filegroup for AttachmentBody.

--- AvvirDatabase/dbo/Tables/AttachmentBody.sql
+++ AvvirDatabase/dbo/Tables/AttachmentBody.sql
@@ -1,7 +1,11 @@
 ﻿CREATE TABLE [dbo].[AttachmentBody] (
-    [GUID]       UNIQUEIDENTIFIER CONSTRAINT [DF_AttachmentBody_GUID] DEFAULT (newsequentialid()) NOT NULL,
-    [HeaderGUID] UNIQUEIDENTIFIER NOT NULL,
-    [Body]       VARBINARY (MAX)  NOT NULL,
-    CONSTRAINT [PK_AttachmentBody] PRIMARY KEY CLUSTERED ([GUID] ASC)
-);
+    [GUID]       UNIQUEIDENTIFIER           CONSTRAINT [DF_AttachmentBody_GUID] DEFAULT (newsequentialid()) NOT NULL,
+    [HeaderGUID] UNIQUEIDENTIFIER           NOT NULL,
+    [RowGUIDCol] UNIQUEIDENTIFIER           CONSTRAINT [DF_AttachmentBody_RowGUIDCol] DEFAULT (newsequentialid()) ROWGUIDCOL NOT NULL,
+    [Body]       VARBINARY (MAX) FILESTREAM NOT NULL,
+    CONSTRAINT [PK_AttachmentBody] PRIMARY KEY CLUSTERED ([GUID] ASC),
+    CONSTRAINT [rowguidcol_unique] UNIQUE NONCLUSTERED ([RowGUIDCol] ASC)
+) FILESTREAM_ON [UserAttachments];
+
+
 
