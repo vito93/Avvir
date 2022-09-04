07dfd95176fe126e1224c706b6e9c4a6b30f700f
Victor Klein <vikklein93@gmail.com>
2022-09-04 21:45:01 +0400

    Comments logic initiated

---
+++ AvvirDatabase/dbo/Tables/Comment.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Comment] (
+    [GUID]        UNIQUEIDENTIFIER CONSTRAINT [DF_Comment_GUID] DEFAULT (newsequentialid()) NOT NULL,
+    [MessageGUID] UNIQUEIDENTIFIER NOT NULL,
+    [AuthorGUID]  UNIQUEIDENTIFIER NOT NULL,
+    [Body]        NVARCHAR (1000)  NOT NULL,
+    [Created]     DATETIME         NOT NULL,
+    CONSTRAINT [PK_Comment] PRIMARY KEY CLUSTERED ([GUID] ASC),
+    CONSTRAINT [FK_Comment_Account] FOREIGN KEY ([AuthorGUID]) REFERENCES [dbo].[Account] ([GUID]),
+    CONSTRAINT [FK_Comment_Message] FOREIGN KEY ([MessageGUID]) REFERENCES [dbo].[Message] ([GUID])
+);
+
