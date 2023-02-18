a84c6e6d17d9c269d750a5d1dcf0529db439b428
Ekostromin <Ekostromin@IBS>
2023-02-18 15:38:46 +0400

    Desktop версия

---
+++ Avvir.Desktop/UserControls/ChatSeparator.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Avvir.Desktop.UserControls
+{
+    public partial class ChatSeparator : UserControl
+    {
+        public ChatSeparator()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(ChatSeparator));
+    }
+}
