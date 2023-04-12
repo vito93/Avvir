fe64c5ef7c9ca928093296426566e3156f6057f7
Victor Klein <vikklein93@gmail.com>
2023-04-12 16:26:58 +0400

    Beginning of the token controller

---
+++ Avvir.Middleware.Core/Controllers/TokenController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Avvir.Middleware.Core.IdentityAuth;
+using Microsoft.AspNetCore.Identity;
+using Avvir.Middleware.Core.Models;
+
+namespace Avvir.Middleware.Core.Controllers
+{
+    [ApiController]
+    public class TokenController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("token/generate")]
+        public IActionResult Login(LoginModel lm)
+        {
+            var authClaims = new List<Claim>
+                {
+                new Claim(ClaimTypes.Name, lm.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:SecretKey"]));
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                expires: DateTime.Now.AddHours(5),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                expiration = token.ValidTo
+            });
+        }
+    }
+}
