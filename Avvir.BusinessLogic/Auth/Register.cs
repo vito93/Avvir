1838afacef0989270b70d6dfcad630ea983d924c
Victor Klein <vikklein93@gmail.com>
2023-04-16 22:01:37 +0400

    Beginning of the registration logic

---
+++ Avvir.BusinessLogic/Auth/Register.cs
@@ -0,0 +1,50 @@
+﻿using Avvir.BusinessLogic.Interfaces;
+using Avvir.BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Avvir.BusinessLogic.Auth
+{
+    internal class Register : ICallabaleBL
+    {
+        public IResult Result { get; }
+        private IResult _result;
+
+        public RegisterModel Model { get; }
+        private RegisterModel _model;
+
+        public Register(RegisterModel model)
+        {
+            _model = model;
+        }
+        public void ProcessLogic()
+        {
+            try
+            {
+                var hp = new HashPassword(_model.Password);
+
+                if (hp.Result.Code == 0)
+                {
+                    //using(var db = new )
+                }
+                else throw new Exception(hp.Result.Message);
+            }
+            catch (Exception ex)
+            {
+                //tba
+            }
+            finally
+            {
+                //tba
+            }
+        }
+    }
+}
---
+++ Avvir.Middleware.Core/Controllers/RegistrationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Avvir.BusinessLogic;
+using Avvir.BusinessLogic.Models;
+
+namespace Avvir.Middleware.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        [HttpPost]
+        [Route("register")]
+        public ActionResult Register([FromBody] RegisterModel input)
+        {
+
+            return Ok();
+        }
+    }
+}
